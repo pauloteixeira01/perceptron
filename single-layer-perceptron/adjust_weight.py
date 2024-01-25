8e698c0beed00693ff36086d4b499364f776a02d
Paulo Teixeira <paulo.teixeira541@gmail.com>
2024-01-24 22:39:38 -0300

    feat: show message ok and outputs.

--- single-layer-perceptron/adjust_weight.py
+++ single-layer-perceptron/adjust_weight.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from status_from_neuron import status_from_neuron
 from to_train import toTrain
+from calc_output import calcOutput
 
 inputs = np.array([[0,0], [0,1], [1,0], [1,1]])
 outputs = np.array([0, 0, 0, 1])
@@ -9,3 +10,8 @@ weights = np.array([0.0, 0.0])
 learningRate = 0.1
 
 toTrain(inputs, outputs, weights, learningRate)
+print('Neural network trained!')
+
+for outputRegister in range(len(outputs)):
+    registers = np.array(inputs[outputRegister])
+    print(calcOutput(registers, weights)) 
