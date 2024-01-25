92b55abe436b94b7e40a88f26108adaa092efc9b
Paulo Teixeira <paulo.teixeira541@gmail.com>
2024-01-24 23:02:49 -0300

    feat: test with operator 'or'

--- single-layer-perceptron/adjust_weight.py
+++ single-layer-perceptron/adjust_weight.py
@@ -4,8 +4,14 @@ from status_from_neuron import status_from_neuron
 from to_train import toTrain
 from calc_output import calcOutput
 
+# and
+# inputs = np.array([[0,0], [0,1], [1,0], [1,1]])
+# outputs = np.array([0, 0, 0, 1])
+
+# or
 inputs = np.array([[0,0], [0,1], [1,0], [1,1]])
-outputs = np.array([0, 0, 0, 1])
+outputs = np.array([0, 1, 1, 1])
+
 weights = np.array([0.0, 0.0])
 learningRate = 0.1
 
